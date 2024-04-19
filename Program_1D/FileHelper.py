d764480254e74bb0dd07d010d96973764653a174
WolfgangPhy <antonin937@gmail.com>
2024-04-19 20:03:01 +0200

    New tests

--- Program_1D/FileHelper.py
+++ Program_1D/FileHelper.py
@@ -15,6 +15,7 @@ class FileHelper:
         plot_filename += f"PN_{thousands_particles}k_"
         plot_filename += f"DS_{parameters['domain_size']}_"
         plot_filename += f"CN_{parameters['cells_number']}_"
+        plot_filename += f"V_{parameters['initial_velocity']}_"
         plot_filename += f"IN_{thousands_iterations}k_"
         
         return plot_filename
